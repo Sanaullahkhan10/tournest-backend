6870d1c68e0ab813ba0a41f2dcfae92a33dd4bd9
Sanaullah khan <sk0198885@gmail.com>
2026-04-29 10:14:23 +0500

    Fix CORS for production and development

--- app.js
+++ app.js
@@ -23,8 +23,20 @@ const viewRouter = require('./routes/viewRoutes');
 // Start express app
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:5174',
+  'https://tournest-frontend.onrender.com'
+];
+
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 
